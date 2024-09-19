d04f0940342ee84ccf2abc0454c6af1556542410
vyas2610 <deepakvyas261092@gmail.com>
2024-09-19 14:36:46 +0530

    Fonts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react'
 import "./css/style.css";
-import { Col, Container, Row, Spinner } from 'react-bootstrap'
+import { Spinner } from 'react-bootstrap'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 const Layout = React.lazy(() => import('./layout/Layout'));
 const HomeScreen = React.lazy(() => import('./screen/Home/HomeScreen'));
