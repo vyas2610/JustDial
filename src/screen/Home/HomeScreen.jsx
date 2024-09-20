1df800201aa4f373ac7a5a78162331ddfb9d4371
vyas2610 <deepakvyas261092@gmail.com>
2024-09-20 15:56:44 +0530

    home

--- src/screen/Home/HomeScreen.jsx
+++ src/screen/Home/HomeScreen.jsx
@@ -6,7 +6,7 @@ const HomeScreen = () => {
     <>
       <div className="container mx-auto py-5 bg-white font-bold">
         <div className="flex px-5 items-center">
-          <div className="flex flex-col">
+          <div className="flex flex-col py-5">
             <div>
               <h1 className="text-2xl">
                 Search across ' 5.6 Crore+'
@@ -14,7 +14,28 @@ const HomeScreen = () => {
               </h1>
             </div>
             <div>
-              <p>a asdsa</p>
+              <div className="lg:flex gap-2">
+                <input
+                  id="location"
+                  name="location"
+                  type="text"
+                  placeholder="Select Location"
+                  autoComplete="location"
+                  className=""
+                />
+
+                <div className="border-2 border-red-700 flex items-center rounded">
+                  <input
+                    id="location"
+                    name="location"
+                    type="text"
+                    placeholder="Search for packages "
+                    autoComplete="location"
+                    className="grow outline-none px-3 py-2"
+                  />
+                  <span>icon</span>
+                </div>
+              </div>
             </div>
           </div>
           <div className="ms-auto border p-3 ">Download App</div>
