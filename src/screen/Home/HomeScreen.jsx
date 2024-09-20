c473e605922fadeee3682504844ac7333295dd77
vyas2610 <deepakvyas261092@gmail.com>
2024-09-20 15:35:58 +0530

    Home Screen

--- src/screen/Home/HomeScreen.jsx
+++ src/screen/Home/HomeScreen.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 const HomeScreen = () => {
   return (
     <>
-        
+      <div className="container mx-auto py-5 bg-white font-bold">
+        <div className="flex px-5 items-center">
+          <div className="flex flex-col">
+            <div>
+              <h1 className="text-2xl">
+                Search across ' 5.6 Crore+'
+                <span className="text-cyan-500">Products & Services</span>
+              </h1>
+            </div>
+            <div>
+              <p>a asdsa</p>
+            </div>
+          </div>
+          <div className="ms-auto border p-3 ">Download App</div>
+        </div>
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
