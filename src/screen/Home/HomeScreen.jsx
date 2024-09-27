b6295d74693a5cb69590ebe9611bad85f3881e94
vyas2610 <deepakvyas261092@.com>
2024-09-27 15:35:40 -0700

    Updates

--- src/screen/Home/HomeScreen.jsx
+++ src/screen/Home/HomeScreen.jsx
@@ -5,6 +5,8 @@ import slider1 from "../../images/slider-1.webp";
 import img1 from "../../images/img1.webp";
 import HomeCategories from "./HomeCategories";
 
+import catimg from "../../images/hkim_banquethall.png";
+
 const HomeScreen = () => {
   return (
     <>
@@ -78,7 +80,143 @@ const HomeScreen = () => {
       </div>
 
       <HomeCategories />
-      
+
+      <div className=" container flex mx-auto bg-white p-5 gap-5">
+        <div className="flex w-1/2 px-5 flex-col border border-gray-300 rounded-lg">
+          <div className="p-2 font-bold">Wedding Requisites</div>
+          <div className="flex f-c gap-3 py-4">
+            <div className="w-1/3">
+              <img src={catimg} alt="" className="wrap" />
+            </div>
+            <div className="w-1/3">
+              <img src={catimg} alt="" className="wrap" />
+            </div>
+            <div className="w-1/3">
+              <img src={catimg} alt="" className="wrap" />
+            </div>
+          </div>
+        </div>
+        <div className="flex w-1/2 px-5 flex-col border border-gray-300 rounded-lg">
+          <div className="p-2 font-bold">Beauty & Spa</div>
+          <div className="flex f-c gap-3 py-4">
+            <div className="w-1/3">
+              <img src={catimg} alt="" className="wrap" />
+            </div>
+            <div className="w-1/3">
+              <img src={catimg} alt="" className="wrap" />
+            </div>
+            <div className="w-1/3">
+              <img src={catimg} alt="" className="wrap" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className=" container flex mx-auto bg-white p-5 gap-5">
+        <div className="flex w-1/2 px-5 flex-col border border-gray-300 rounded-lg">
+          <div className="p-2 font-bold">Wedding Requisites</div>
+          <div className="flex f-c gap-3 py-4">
+            <div className="w-1/3">
+              <img src={catimg} alt="" className="wrap" />
+            </div>
+            <div className="w-1/3">
+              <img src={catimg} alt="" className="wrap" />
+            </div>
+            <div className="w-1/3">
+              <img src={catimg} alt="" className="wrap" />
+            </div>
+          </div>
+        </div>
+        <div className="flex w-1/2 px-5 flex-col border border-gray-300 rounded-lg">
+          <div className="p-2 font-bold">Beauty & Spa</div>
+          <div className="flex f-c gap-3 py-4">
+            <div className="w-1/3">
+              <img src={catimg} alt="" className="wrap" />
+            </div>
+            <div className="w-1/3">
+              <img src={catimg} alt="" className="wrap" />
+            </div>
+            <div className="w-1/3">
+              <img src={catimg} alt="" className="wrap" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Bills & Recharge --- Travel Booking */}
+
+      <div className="container flex flex-col p-5 bg-white mx-auto">
+        <div className="flex border p-4 rounded-lg">
+          <div className="w-1/4 p-4 leading-10">
+            <h1 className="font-bold text-2xl">Bills & Recharge</h1>
+            <p className="leading-6 text-sm">
+              Pay your bill & recharge instantly with justdial
+            </p>
+            <p className="text-sm text-blue-400 font-bold"> Explore More</p>
+          </div>
+          <div className="flex gap-10">
+            <div className="w-1/3 py-2 ">
+              <img src={img1} alt="" width="100" className="border " />
+              <p className="font-bold">Mobile</p>
+            </div>
+            <div className="w-1/3 py-2 ">
+              <img src={img1} alt="" width="100" className="border " />
+              <p className="font-bold">Mobile</p>
+            </div>
+            <div className="w-1/3 py-2 ">
+              <img src={img1} alt="" width="100" className="border " />
+              <p className="font-bold">Mobile</p>
+            </div>
+            <div className="w-1/3 py-2 ">
+              <img src={img1} alt="" width="100" className="border " />
+              <p className="font-bold">Mobile</p>
+            </div>
+            <div className="w-1/3 py-2 ">
+              <img src={img1} alt="" width="100" className="border " />
+              <p className="font-bold">Mobile</p>
+            </div>
+            <div className="w-1/3 py-2 ">
+              <img src={img1} alt="" width="100" className="border " />
+              <p className="font-bold">Mobile</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex border p-4 rounded-lg">
+          <div className="w-1/4 p-4 leading-10">
+            <h1 className="font-bold text-2xl">Travel Booking</h1>
+            <p className="leading-6 text-sm">
+              Instant ticket bookings for your best travel experience
+            </p>
+            <p className="text-sm text-blue-400 font-bold"> Explore More</p>
+          </div>
+          <div className="flex gap-10">
+            <div className="w-1/3 py-2  hover:shadow  ">
+              <img src={img1} alt="" width="100" className="border " />
+              <p className="font-bold">Mobile</p>
+            </div>
+            <div className="w-1/3 py-2 ">
+              <img src={img1} alt="" width="100" className="border " />
+              <p className="font-bold">Mobile</p>
+            </div>
+            <div className="w-1/3 py-2 ">
+              <img src={img1} alt="" width="100" className="border " />
+              <p className="font-bold">Mobile</p>
+            </div>
+            <div className="w-1/3 py-2 ">
+              <img src={img1} alt="" width="100" className="border " />
+              <p className="font-bold">Mobile</p>
+            </div>
+            <div className="w-1/3 py-2 ">
+              <img src={img1} alt="" width="100" className="border " />
+              <p className="font-bold">Mobile</p>
+            </div>
+            <div className="w-1/3 py-2 ">
+              <img src={img1} alt="" width="100" className="border " />
+              <p className="font-bold">Mobile</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
