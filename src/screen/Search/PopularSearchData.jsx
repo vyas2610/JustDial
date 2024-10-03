ac515bacb5bc0a7fe0c27bba30a09c088fd28cb0
vyas2610 <deepakvyas261092@gmail.com>
2024-10-03 11:48:44 +0530

    PopularSearchData

--- src/screen/Search/PopularSearchData.jsx
+++ src/screen/Search/PopularSearchData.jsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import adsImg from "../../images/ads.jpg";
 const PopularSearchData = () => {
-  return <div>PopularSearchData</div>;
+  return (
+    <>
+      <div className=" container mx-auto   bg-white">
+        <img src={adsImg} alt="" srcset="" className="w-full" />
+
+        {/* Heading & Filters */}
+        <div className="flex py-5 p-5 flex-col">
+          <div>
+            <p className="text-gray-400 text-sm">
+              Jodhpur | Interior Designers in Jodhpur | 526+ Listings
+            </p>
+          </div>
+          <div>
+            <h1 className="font-bold text-2xl">
+              Popular Interior Designers in Jodhpur
+            </h1>
+          </div>
+          <div></div>
+        </div>
+
+        {/* Data  */}
+      </div>
+    </>
+  );
 };
 
 export default PopularSearchData;
